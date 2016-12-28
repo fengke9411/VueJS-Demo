8edb0a9c6d9ad2459fd115d5ade8c2a476b56aa3
fengkk <fengke9411@qq.com>
2016-12-28 14:38:07 +0800

    添加代码

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+
+<template>
+	<div id="login">
+		<input v-model="userName" />
+		<input v-model="userPwd" />
+	</div>
+
+
+</template>
+
+
+<script>
+	var vue = new Vue({
+		el:"#login",
+		data:{
+			userName:"",
+			userPwd:""
+		},
+		created:function(){
+			console.log("view is created!")
+		},
+		mounted:function(){
+			console.log("view is mounted!")
+		},
+		updated:function(){
+			console.log("view is updated!")
+		},
+		destoryed:function(){
+			console.log("view is destoryed!")
+		}
+
+	});
+
+</script>
+
+<style>
+	
+</style>
